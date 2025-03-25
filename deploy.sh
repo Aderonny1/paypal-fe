ccbff6d086db987f3cfd1a869b3ffd8e235a2b58
Joy <ogundipeaderonkejoy@gmail.com>
2025-03-25 18:45:27 +0000

    deployment feature modified

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 Developers are at work
 Application is ready to be deployed to the development enivronment.
 development must be reviewed by managers before deployment
-SDLC is great
 AWS CodeDeploy is a great tool.
+SDLC is great
+deployment with ansible
